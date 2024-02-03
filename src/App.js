c6a6cd3405af15360783b36681fefa17509a7340
YoungGuy2003 <abhijithts402@gmail.com>
2024-02-03 14:09:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Carousel from './components/Carousel';
+import Carousel2 from './components/Carousel2';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Carousel />
+      <Carousel2 />
+    </div>
+  );
+};
+
+export default App;
